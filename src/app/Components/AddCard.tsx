8be4fc3ca3b83cf29a55aad8e98e054e7933c82d
lucas <lucasagustinastesiano@gmail.com>
2025-03-30 23:00:36 -0300

    cambio react-credit-card a su version 2

--- src/app/Components/AddCard.tsx
+++ src/app/Components/AddCard.tsx
@@ -1,6 +1,6 @@
 'use client'
 import React, { useState } from "react";
-import Cards from "react-credit-cards";
+import Cards from "react-credit-cards-2";
 import "react-credit-cards/es/styles-compiled.css";
 import { CardServices } from "../api/cards/CardServices";
 
